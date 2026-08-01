28b242f007e2652065a47a4c76831447c435d570
barttoni52-cloud <barttoni52@gmail.com>
2026-08-01 09:11:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import ValidatePage from './components/ValidatePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/validate/:token" element={<ValidateRoute />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+function ValidateRoute() {
+  const token = window.location.pathname.split('/validate/')[1];
+  return <ValidatePage token={token} />;
+}
